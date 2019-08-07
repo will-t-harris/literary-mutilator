4bc50c4f354b5f24cda677cad06db13030a40fe8
Will Harris <will.t.harris@protonmail.com>
2019-08-07 11:12:07 -0600

    convert all paragraphs into one long string, split into large array of each word

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
 const paragraphs = document.getElementsByClassName('paragraph');
 const paragraphArray = Array.from(paragraphs);
+let secondParagraph = paragraphArray[1].textContent;
 
 //Highlight entire paragraph
 document.getElementById('test').onclick = () => {
 	document.getElementById('test').classList.toggle('active');
 };
 
-// Highlight individual word on click
-// const container = document.getElementById('container');
-// container.addEventListener('click', event => {
-// 	const target = `${event.offsetX} ${event.offsetY}`;
-// 	console.log(target);
-// });
-//
-
 // Remove word on scroll
 let paragraphString = [].map.call(paragraphs, (node) => {
 	return node.textContent;
 }).join("");
+
+console.log(paragraphString.split(' '));
