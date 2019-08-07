5da619573fd847a4ef2c200abe7f88a6c69760de
Will Harris <will.t.harris@protonmail.com>
2019-08-07 14:47:09 -0600

    add comments, figure out shuffle function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-const paragraphs = document.getElementsByClassName('paragraph');
-const paragraphArray = Array.from(paragraphs);
-let secondParagraph = paragraphArray[1].textContent;
+const paragraphs = document.getElementsByClassName('paragraph'); // this is a HTML collection
+const paragraphArray = Array.from(paragraphs); // Create array from HTML collection
+let secondParagraph = paragraphArray[1].textContent; // Select only the second paragraph
 
-//Highlight entire paragraph
+//Highlight entire paragraph on click
 document.getElementById('test').onclick = () => {
 	document.getElementById('test').classList.toggle('active');
 };
 
-// Remove word on scroll
+// Shuffle words in random order function
+function shuffle(array) {
+	for(let i = 0; i < array.length; i++) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+}
+
+// form string from all the paragraphs
 let paragraphString = [].map.call(paragraphs, (node) => {
 	return node.textContent;
 }).join("");
 
-console.log(paragraphString.split(' '));
+// form array of all the words from the paragraph string
+const wordsArray = paragraphString.split(' ');
+window.addEventListener('scroll', e => {
+	console.log(e);
+});
