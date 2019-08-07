6b2ec7e8d6ee48e2eb0931e718ea9b75a1f14e34
Will Harris <will.t.harris@protonmail.com>
2019-08-07 09:37:13 -0600

    comment out code not being worked on

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-// Highlight entire paragraph
 const paragraphs = document.getElementsByClassName('paragraph');
-
 const paragraphArray = Array.from(paragraphs);
-paragraphArray.forEach(element => element.style.background = "yellow");
+
+//Highlight entire paragraph
+document.getElementById('test').onclick = () => {
+	document.getElementById('test').style.background = "yellow";
+};
+
+// Highlight individual word on click
+// const container = document.getElementById('container');
+// container.addEventListener('click', event => {
+// 	const target = `${event.offsetX} ${event.offsetY}`;
+// 	console.log(target);
+// });
+//
+// // Find & Replace
+// findAndReplace = (findWord, replaceWord) => {
+//
+// }
